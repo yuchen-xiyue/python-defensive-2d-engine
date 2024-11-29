f172e4f81e6535aeede6d38d4ae0070c7dfcc71e
yuchen-xiyue <3450045220@qq.com>
2024-11-29 15:48:31 +0000

    Updated comments in main.py

--- main.py
+++ main.py
@@ -5,27 +5,21 @@ from libs import engine
 SCRIPT_DIRECTORY = "scripts"
 
 def main() -> None: 
+    # Parse user input
     parser = argparse.ArgumentParser()
     parser.add_argument("script", type=str, help="Type in your script filename. ")
     args = parser.parse_args()
-
+    # Load script from file
     script_path = os.path.join(SCRIPT_DIRECTORY, args.script)
     script_loader = engine.ScriptLoader(filename=script_path)
     script_loader.read_script()
     height, width = script_loader.get_size()
     shapes = script_loader.get_shapes()
-
     buffer = engine.Buffer(height=height, width=width)
-
-    try: 
-
-        for shape in shapes: 
-            shape.draw(canvas=buffer)
-
-    except Exception as e: 
-
-        print(e)
-
+    # Draw frame to buffer
+    for shape in shapes: 
+        shape.draw(canvas=buffer)
+    # Display 
     buffer.display(mode='square')
 
 
